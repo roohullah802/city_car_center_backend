4fb99ed3c536253a57c5a070c751db258bcabac3
roohullah802 <jaanroohullah83@gmail.com>
2025-08-26 10:40:30 -0700

    changes

--- src/lib/multer/multer.ts
+++ src/lib/multer/multer.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, '../../../private_data/uploads');
+    cb(null, '../../../../private_data/uploads');
   },
   filename: (req, file, cb) => {
     const uniqueName = Date.now() + "-" + Math.round(Math.random() * 1e9);
