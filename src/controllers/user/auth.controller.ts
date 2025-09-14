797897f42e39d4831b3f617095f2261c5f80c0ae
roohullah802 <jaanroohullah83@gmail.com>
2025-09-13 21:18:48 -0700

    bug fix

--- src/controllers/user/auth.controller.ts
+++ src/controllers/user/auth.controller.ts
@@ -96,6 +96,34 @@ export async function appleAuth(req: Request, res: Response): Promise<void> {
   }
 }
 
+
+
+
+export const validateToken = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader){
+      res.status(401).json({ message: "No token" });
+      return;
+    }
+
+    const token = authHeader.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
+
+    const user = await User.findById(decoded?.userId);
+    if (!user){
+      res.status(404).json({ message: "User not found" });
+      return;
+    }
+
+    res.status(200).json({success: true,user});
+  } catch (err) {
+    res.status(401).json({ message: "Invalid or expired token" });
+    return;
+  }
+};
+
+
 /**
  * @route   POST /api/auth/signup
  * @desc    Register a new user
--- src/routers/user/auth.router.ts
+++ src/routers/user/auth.router.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { userLogout, userProfile, googleAuth, appleAuth } from '../../controllers/user/auth.controller'
+import { userLogout, userProfile, googleAuth, appleAuth, validateToken } from '../../controllers/user/auth.controller'
 import { authMiddleware } from '../../middleware/auth.middleware'
 import uploadPDF from '../../lib/multer/pdf.multer'
 
@@ -9,7 +9,8 @@ const userAuthRouter = express.Router()
 
 userAuthRouter.route("/google").post(googleAuth);
 userAuthRouter.route("/apple").post(appleAuth);
-userAuthRouter.route("/logout").post(authMiddleware, userLogout)
-userAuthRouter.route("/update/profile").post(authMiddleware,uploadPDF.single('file'), userProfile)
+userAuthRouter.route("/validate").get(validateToken);
+userAuthRouter.route("/logout").post(authMiddleware, userLogout);
+userAuthRouter.route("/update/profile").post(authMiddleware,uploadPDF.single('file'), userProfile);
 
 export { userAuthRouter }
