3ff3ecf53c943e728d53c87f4321f3c84cb402cf
roohullah802 <jaanroohullah83@gmail.com>
2025-09-18 20:26:29 -0700

    bug fix

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import { connectDB } from "./src/db/mongodb";
@@ -8,11 +8,16 @@ import cookieParser from "cookie-parser";
 import { userAuthRouter } from "./src/routers/user/auth.router";
 import { userRouter } from "./src/routers/user/user.router";
 import { adminRouter } from "./src/routers/admin/admin.router";
-import { connectRedis } from "./src/lib/redis/redis";
+import { connectRedis, redisClient } from "./src/lib/redis/redis";
 import "./src/lib/mail/reminder/leaseReminderWorker";
 import { leaseReminderQueue } from "./src/lib/mail/reminder/leaseReminderQueue";
 import "./src/lib/mail/email.Processor";
 import paymentRoutes from './src/routers/payment/payment'
+import Stripe from "stripe";
+import { Lease } from "./src/models/Lease.model";
+import { emailQueue } from "./src/lib/mail/emailQueues";
+import { Car } from "./src/models/car.model";
+
 
 dotenv.config();
 connectRedis();
@@ -33,13 +38,94 @@ const corsOptions = {
   credentials: true, // If you're using cookies or auth headers
 };
 
-app.use(
-  (req, res, next) => {
-    if (req.originalUrl === "/api/payment/webhook") {
-      next();
-    } else {
-      bodyParser.json()(req, res, next);
+// app.use(
+//   (req, res, next) => {
+//     if (req.originalUrl === "/api/payment/webhook") {
+//       next();
+//     } else {
+//       bodyParser.json()(req, res, next);
+//     }
+//   }
+// );
+
+const stripe = new Stripe(process.env.STRIPE_SERVER_KEY as string, {
+  apiVersion: "2025-05-28.basil",
+});
+
+app.post("/webhook",express.raw({ type: "application/json" }),async (req: Request, res: Response): Promise<void> => {
+    const sig = req.headers["stripe-signature"] as string;
+
+    let event: Stripe.Event;
+    try {
+      event = stripe.webhooks.constructEvent(
+        req.body,
+        sig,
+        process.env.STRIPE_WEBHOOK_SECRET as string
+      );
+    } catch (err: any) {
+      res.status(400).send(`Webhook Error: ${err.message}`);
+      return;
     }
+
+    if (event.type === "payment_intent.succeeded") {
+      const paymentIntent = event.data.object as Stripe.PaymentIntent;
+      console.log('payment is created sucessfully');
+      
+
+      // // ✅ Read metadata values
+      const { userId, carId, startDate, endDate, email } = paymentIntent.metadata;
+
+      let lease;
+      if (userId && carId) {
+       lease =  await Lease.create({
+          user: userId,
+          car: carId,
+          amount: paymentIntent.amount / 100,
+          paymentIntentId: paymentIntent.id,
+          status: "completed",
+          startDate: new Date(startDate),
+          endDate: new Date(endDate),
+        });
+
+        await Car.findByIdAndUpdate(carId, {
+            available: false
+        }, {new: true});
+
+        const redisCars = await redisClient.get('AllCars:AllCars');
+        if (redisCars) {
+            const allCars = JSON.parse(redisCars);
+            const cars = allCars.find((c:any)=> c._id === carId);
+            if (cars) {
+                cars.available = false
+                return cars
+            }
+        }
+        await redisClient.hSet(`carDetails:${carId}`, 'available', 'false');
+        await redisClient.del(`leasePaymentHistory:${userId}`);
+        console.log('lease created success!!');
+        
+
+        await emailQueue.add(
+              "leaseConfirmationEmail",
+              { leaseId: lease._id, startDate, endDate, to: email },
+              {
+                attempts: 3,
+                backoff: {
+                  type: "exponential",
+                  delay: 5000,
+                },
+              }
+            );
+
+            
+
+
+      }
+    }
+    if (event.type === "payment_intent.payment_failed") {
+      console.log('payment is failed!!');
+    }
+    res.json({ received: true });
   }
 );
 
