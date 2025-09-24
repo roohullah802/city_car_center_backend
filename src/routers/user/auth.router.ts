8772f712fccf7ce64087e7ba8fe4ca2c1644d4a9
roohullah802 <jaanroohullah83@gmail.com>
2025-09-23 18:31:00 -0700

    bug fix

--- src/routers/user/auth.router.ts
+++ src/routers/user/auth.router.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { userLogout, userProfile, googleAuth, appleAuth, validateToken } from '../../controllers/user/auth.controller'
+import {  googleAuth, appleAuth, validateToken } from '../../controllers/user/auth.controller'
 import { authMiddleware } from '../../middleware/auth.middleware'
 import uploadPDF from '../../lib/multer/pdf.multer'
 
@@ -10,7 +10,4 @@ const userAuthRouter = express.Router()
 userAuthRouter.route("/google").post(googleAuth);
 userAuthRouter.route("/apple").post(appleAuth);
 userAuthRouter.route("/validate").post(validateToken);
-userAuthRouter.route("/logout").post(authMiddleware, userLogout);
-userAuthRouter.route("/update/profile").post(authMiddleware,uploadPDF.single('file'), userProfile);
-
 export { userAuthRouter }
